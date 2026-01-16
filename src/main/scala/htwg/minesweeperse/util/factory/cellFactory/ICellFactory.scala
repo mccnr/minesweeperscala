bf0b5958ad17c4db454156af32a42a384573b634
mccnr <imelcin80@web.de>
2026-01-16 15:51:01 +0100

    finalfix

--- src/main/scala/htwg/minesweeperse/util/factory/cellFactory/ICellFactory.scala
+++
@@ -1,7 +0,0 @@
-package htwg.minesweeperse.util.factory.cellFactory
-
-import htwg.minesweeperse.model.cell.Cell
-
-trait ICellFactory:
-  def create(value: Int): Cell
-
--- src/main/scala/htwg/minesweeperse/util/factory/revealFactory/IRevealFactory.scala
+++
@@ -1,6 +0,0 @@
-package htwg.minesweeperse.util.factory.revealFactory
-
-import htwg.minesweeperse.util.strategy.revealComponent.impl.IRevealStrategy
-
-trait IRevealFactory:
-  def create(): IRevealStrategy
