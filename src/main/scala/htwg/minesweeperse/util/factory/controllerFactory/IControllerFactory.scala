5d37ee5bcc74dce33415e093cf01e377246a6f91
mccnr <imelcin80@web.de>
2026-01-16 15:45:59 +0100

    fix

--- src/main/scala/htwg/minesweeperse/util/factory/controllerFactory/IControllerFactory.scala
+++
@@ -1,8 +0,0 @@
-package htwg.minesweeperse.util.factory.controllerFactory
-
-import htwg.minesweeperse.controllerComponent.impl.IController
-import htwg.minesweeperse.model.fieldComponent.impl.IField
-import htwg.minesweeperse.util.strategy.revealComponent.impl.IRevealStrategy
-
-trait IControllerFactory:
-  def create(field: IField, reveal: IRevealStrategy): IController
